755bbb1b92cfa1190f00d935e13f5e73c46206f9
eaziwizzy <wisdomakatugba1@gmail.com>
2023-09-07 18:21:43 -0700

    print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the function nam
+ *
+ * Return: the return value is 0
+*/
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
