7145c9505c5602bec09b4d7fda67b1ecf4d2a95a
eaziwizzy <wisdomakatugba1@gmail.com>
2023-09-07 17:42:25 -0700

    first task working with betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the func name
+ *
+ * Return: the type is int and returns 0
+*/
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual");
+	return (0);
+}
