4cf9224466978b15df9c7db50fe16c3636051cfb
eaziwizzy <wisdomakatugba1@gmail.com>
2023-09-07 19:41:51 -0700

    different types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ int main(void)
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(long longer));
+	printf("Size of a float: %zu byte(s)\n", sizeof(long long));
 	return (0);
 }
