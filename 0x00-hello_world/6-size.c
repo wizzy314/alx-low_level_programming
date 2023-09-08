c96b2189b33c54d6e6368103aa3cf09ca1b7199e
eaziwizzy <wisdomakatugba1@gmail.com>
2023-09-07 19:38:07 -0700

    different types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - function's name
+ *
+ * Return: it's successful if it returns 0
+*/
+
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(long longer));
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains my learning journey of C programme. 
