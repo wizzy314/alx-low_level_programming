bc48de07665b72a866687b068cdd85c16d8ca7ed
eaziwizzy <wisdomakatugba1@gmail.com>
2023-09-08 19:11:07 -0700

    for loop to list 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - functon name type
+ *
+ * Return: successfully 0
+*/
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
