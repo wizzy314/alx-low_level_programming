a6409f7a6c74e455753777b6c18aa1aed2a1d1ba
eaziwizzy <wisdomakatugba1@gmail.com>
2023-09-13 06:28:24 -0700

    solution to task 9 to 104

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,55 +1,36 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table.
+ * times_table - prints the 9 times table, starting with 0
  */
-
-
 void times_table(void)
 {
+	int i, j, k;
 
-	char row, column, result;
-
-	for (row = 0; row <= 9; row++)
-/*Loop through rows (0 to 9)*/
+	for (i = 0; i < 10; i++)
 	{
-
-		for (column = 0; column <= 9; column++)
-/*Loop through columns (0 to 9)*/
+		for (j = 0; j < 10; j++)
 		{
-
-			result = (row * column);
-/*Calculate the product*/
-
-			if (column > 0)
+			k = j * i;
+			if (j == 0)
 			{
-
-				_putchar(',');
-/*Print comma separator*/
-_putchar(' ');
-/*Print space*/
+				_putchar(k + '0');
 			}
 
-		if (result < 10 || result > 10)
-		{
-			_putchar(' ');
-/*Print space for single-digit results*/
-			_putchar(result + '0');
-/*Print the result as a character*/
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-
-		else
-		{
-
-		_putchar((result / 10) + '0');
-/*Print tens digit as a character*/
-		_putchar((result % 10) + '0');
-/*Print ones digit as a character*/
-
-		}
-		}
-
-	_putchar('\n');
-/* Print newline after each row*/
+		_putchar('\n');
 	}
 }
