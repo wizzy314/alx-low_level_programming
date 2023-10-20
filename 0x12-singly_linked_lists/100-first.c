e0ab1916229fe66dae46108b71d3ad986a24a2de
eaziwizzy <wisdomakatugba1@gmail.com>
2023-10-19 17:17:17 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-
-void before_main(void) __attribute__ ((constructor));
+#include "lists.h"
 
 /**
  * before_main - Function to be executed before main.
@@ -10,17 +8,4 @@ void before_main(void)
 {
 			printf("You're beat! and yet, you must allow,\nI ");
 			printf("bore my house upon my back!\n");
-}
-
-/**
- * main - The main function.
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-			printf("(A tortoise, having pretty good sense of a hare's ");
-			printf("nature, challenges one to a race.)\n");
-			return (0);
 }
