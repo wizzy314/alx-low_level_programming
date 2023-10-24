b828cad754e2c8a4a22c2d7771140eff7e53b269
eaziwizzy <wisdomakatugba1@gmail.com>
2023-10-24 15:56:29 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,15 +6,17 @@
  *
  * Return: number of nodes
  */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t num = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        num++;
-        h = h->next;
-    }
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
 
-    return (num);
+	return (count);
 }
+
