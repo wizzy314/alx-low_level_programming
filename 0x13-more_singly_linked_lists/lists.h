a6c00c6021f03538d335b8f43c89d43997390ab8
eaziwizzy <wisdomakatugba1@gmail.com>
2023-10-24 16:20:26 -0700

    _putchar.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -12,14 +13,16 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *
  */
+
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
